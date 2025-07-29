8f43af67a6e248905dbd0965c53cd9dfe7ae4dbd
Szymon Kublin <szymonkublin@gmail.com>
2025-07-29 18:21:36 +0200

    Fix - env variables handling

--- app/database.py
+++ app/database.py
@@ -1,8 +1,13 @@
+import os
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = ""
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
