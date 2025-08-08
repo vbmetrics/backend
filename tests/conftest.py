36e4cd6859b13dac39c73238155f363312a29cce
Szymon Kublin <szymonkublin@gmail.com>
2025-08-08 12:28:55 +0200

    Feature - pytest for api testing init

---
+++ tests/conftest.py
@@ -0,0 +1,39 @@
+import pytest
+from typing import Generator
+
+from fastapi.testclient import TestClient
+from sqlmodel import Session, SQLModel, create_engine
+
+from app.main import app
+from app.database import get_session
+
+
+DATABASE_URL_TEST = "sqlite:///./test.db"
+engine = create_engine(
+    DATABASE_URL_TEST, 
+    connect_args={ "check_same_thread": False }
+)
+
+@pytest.fixture(scope="function", autouse=True)
+def create_test_tables():
+    # create tables before testing
+    SQLModel.metadata.create_all(engine)
+    yield
+    # delete tables after testing
+    SQLModel.metadata.drop_all(engine)
+
+@pytest.fixture(name="session")
+def session_fixture() -> Generator[Session, None, None]:
+    with Session(engine) as session:
+        yield session
+
+@pytest.fixture(name="client")
+def client_fixture(session: Session) -> Generator[TestClient, None, None]:
+    def get_session_override() -> Session:
+        return session
+
+    app.dependency_overrides[get_session] = get_session_override
+    
+    yield TestClient(app)
+    
+    app.dependency_overrides.clear()
---
+++ tests/test_countries.py
@@ -0,0 +1,25 @@
+from fastapi.testclient import TestClient
+
+def test_create_and_read_country(client: TestClient):
+    response = client.post(
+        "/api/v1/countries/",
+        json={
+            "name": "Poland",
+            "alpha_2_code": "PL",
+            "latitude": 51.919438,
+            "longitude": 19.145136
+        }
+    )
+    data = response.json()
+
+    assert response.status_code == 201
+    assert data["name"] == "Poland"
+    assert data["alpha_2_code"] == "PL"
+
+    country_code = data["alpha_2_code"]
+    response = client.get(f"/api/v1/countries/{country_code}")
+    
+    assert response.status_code == 200
+    data = response.json()
+    assert data["name"] == "Poland"
+    assert data["alpha_2_code"] == "PL"
